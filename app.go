6546facb2921914fe332c1a9517ae8c3385113cb
Ved Zhadev <emmanuelaajayi2@gmail.com>
2025-02-12 20:48:34 +0100

    Initial commit

--- app.go
+++ app.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, Go!")
+	name := "Naz"
+	author := "Ved ZhaDev"
+
+	pointerAddress(&name, &author)
+	// fmt.Println("Switching Places, we get! ", &n)
+}
+
+func pointerAddress(n *string, a *string) {
+	fmt.Println("The Address on the memory is ", n)
+	fmt.Println("The Address on the memory is ", a)
 }
