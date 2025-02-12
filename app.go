f444419f5b58d703f22c7ea2d2891638816ff831
Ved Zhadev <emmanuelaajayi2@gmail.com>
2025-02-12 21:30:00 +0100

    Made sure Function ran well

--- app.go
+++ app.go
@@ -4,13 +4,20 @@ import "fmt"
 
 func main() {
 	name := "Naz"
-	author := "Ved ZhaDev"
 
-	pointerAddress(&name, &author)
+	var Author string
+	Author = "Ved ZhaDev"
+
+	pointerAddress(&name, Author)
 	// fmt.Println("Switching Places, we get! ", &n)
 }
 
-func pointerAddress(n *string, a *string) {
-	fmt.Println("The Address on the memory is ", n)
-	fmt.Println("The Address on the memory is ", a)
+func pointerAddress(n *string, a string) {
+	//See only the n is a string!
+	fmt.Println("The Address on the memory is ", n, "And the value is; Name:", *n)
+	// fmt.Println("The Address on the memory is ", a, "And the value is; Author:", a)
+	// now we switch
+	*n = a
+
+	fmt.Println("User switched to", *n, "now.")
 }
