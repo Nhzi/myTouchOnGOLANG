415b9dee22e1fb41ce4e3d83666fbff8dc8ba6e5
Ved Zhadev <emmanuelaajayi2@gmail.com>
2025-02-13 08:26:51 +0100

    Lol, just touches

--- control-structures/main.go
+++ control-structures/main.go
@@ -4,18 +4,19 @@ import "fmt"
 
 func main() {
 	var num int
-	num = 9
+	num = 0
 
 	if num == 0 {
 		fmt.Println("WTF do you want me to do with", num)
+	} else if num == 9 {
+		fmt.Println("ouuuuuuu", num, "I can work with that, I would jus need '11' at the back")
 	} else {
-		fmt.Println("ouuuuuuu", num, "I can work with that")
+		fmt.Println("Lets see.")
 	}
 
 	name := "Sheriff"
 
 	authenticator(name, num)
-
 }
 
 func authenticator(name string, num int) {
