46665c5d39ad8a26290d0328677a15be1c13a2fa
Ved Zhadev <emmanuelaajayi2@gmail.com>
2025-02-14 07:55:12 +0100

    Wuruwuru lol

--- 04-control-structures-for-loops/main.go
+++ 04-control-structures-for-loops/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	//Beginning of FOR loop classes
+	//Beginning of FOR loop
+
+	i := 0
+	for i <= 12 {
+		fmt.Println("The square of i at", i, "is", i*i)
+		i++
+	}
+	//Second for loop
+	for x := -1; x >= -12; x-- {
+		fmt.Println("The exponetial of value x at", x, "is", math.Pow(float64(x), float64(x)))
+	}
 }
